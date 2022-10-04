f2726bc1e308c99e17592c0a38429510d360e899
hazal ozer <hazalozer07@gmail.com>
2022-10-05 01:26:12 +0300

    v1

--- src/CinBurcu.java
+++ src/CinBurcu.java
@@ -1,2 +1,53 @@
-package PACKAGE_NAME;public class CinBurcu {
+import java.util.Scanner;
+
+public class CinBurcu {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Lütfen doğum yılınızı giriniz: ");
+        short dogumYili = scan.nextShort();
+
+        switch (dogumYili % 12) {
+            case 0:
+                System.out.println("Maymun");
+                break;
+            case 1:
+                System.out.println("Horoz");
+                break;
+            case 2:
+                System.out.println("Köpek");
+                break;
+            case 3:
+                System.out.println("Domuz");
+                break;
+            case 4:
+                System.out.println("Fare");
+                break;
+            case 5:
+                System.out.println("Öküz");
+                break;
+            case 6:
+                System.out.println("Kaplan");
+                break;
+            case 7:
+                System.out.println("Tavşan");
+                break;
+            case 8:
+                System.out.println("Ejderha");
+                break;
+            case 9:
+                System.out.println("Yılan");
+                break;
+            case 10:
+                System.out.println("At");
+                break;
+            case 11:
+                System.out.println("Koyun");
+                break;
+            default:
+                System.out.println("Hatalı bilgi girişi");
+        }
+
+    }
 }
